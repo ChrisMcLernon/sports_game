750713bbd7c769524c46b743492d7f5000cfb637
Chris McLernon <c.r.mclernon@gmail.com>
2024-07-16 17:46:58 -0400

    modified:   cs/src/Services/JsonReader.cs

--- cs/src/Services/JsonReader.cs
+++ cs/src/Services/JsonReader.cs
@@ -5,15 +5,15 @@ namespace sports_game.src.Services
 {
     public class JsonReader
     {
-        private static readonly string _basePath = Path.GetFullPath("C:/CodingDir/sports_game/cs/src/Data");
+        private static readonly string BASE_PATH = Path.GetFullPath("C:/CodingDir/sports_game/cs/src/Data");
 
         private static void ValidateBasePath(string relativePath)
         {
-            string fullPath = Path.GetFullPath(Path.Combine(_basePath, relativePath));
+            string fullPath = Path.GetFullPath(Path.Combine(BASE_PATH, relativePath));
 
-            if (!fullPath.StartsWith(_basePath, StringComparison.OrdinalIgnoreCase))
+            if (!fullPath.StartsWith(BASE_PATH, StringComparison.OrdinalIgnoreCase))
             {
-                throw new SecurityException($"Base path must be within {_basePath}.");
+                throw new SecurityException($"Base path must be within {BASE_PATH}.");
             }
         }
 
@@ -21,7 +21,7 @@ namespace sports_game.src.Services
         {
             ValidateBasePath(relativePath);
 
-            string fullPath = Path.GetFullPath(Path.Combine(_basePath, relativePath));
+            string fullPath = Path.GetFullPath(Path.Combine(BASE_PATH, relativePath));
 
             string jsonString = File.ReadAllText(fullPath);
             return JsonSerializer.Deserialize<T>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? throw new InvalidOperationException("Deserialization returned null.");
