727835525fa6bc37317e07a1e28666aff27af8bd
chris-mclernon <c.r.mclernon@gmail.com>
2024-07-23 01:12:36 -0400

    modified:   cs/src/Handlers/PlanningHandler.cs

--- cs/src/Handlers/PlanningHandler.cs
+++ cs/src/Handlers/PlanningHandler.cs
@@ -10,14 +10,91 @@ namespace sports_game.src.Handlers
 
         public void PlanningInterface()
         {
-            Console.WriteLine($"\n\nRound {gameHandler.Round}");
-            Console.WriteLine($"Opponent: {gameHandler.OpponentTeam.Name}");
-            Console.WriteLine($"Potential Score: {gameHandler.CalculateScore(true)[1]} (+/-)");
-            Console.WriteLine($"Choose your order:");
-            Lineup = ChooseLineup();
+            while (true)
+            {
+                Console.WriteLine($"\n\nRound {gameHandler.Round}");
+                Console.WriteLine($"Opponent: {gameHandler.OpponentTeam.Name}");
+                Console.WriteLine($"Potential Score: {gameHandler.CalculateScore(true)[1]} (+/-)");
+                Console.WriteLine("1. Choose Lineup and Start");
+                Console.WriteLine("2. Medical Attention");
+
+                switch (InputReader.ReadText("Choose Option: "))
+                {
+                    case "1":
+                        Lineup = ChooseLineup();
+                        return;
+
+                    case "2":
+                        MedicalAttention();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
         }
 
-        private Stack<Person> ChooseLineup()
+        private void MedicalAttention()
+        {
+            List<Person> injuredPlayers = [];
+            while (true)
+            {
+                foreach (Person player in gameHandler.PlayerTeam.Players)
+                {
+                    if (player.Status == "Injured")
+                    {
+                        injuredPlayers.Add(player);
+                        Console.Write(injuredPlayers.Count + ". ");
+                        player.PrintInfo();
+                        Console.WriteLine($"Cost To Give Aid: {player.Cost / 2}");
+                    }
+                }
+                
+                if (injuredPlayers.Count == 0)
+                {
+                    Console.WriteLine("No injured players.");
+                    return;
+                }
+
+                int choice = Convert.ToInt32(InputReader.ReadText("Choose player to give Medical Atention: "));
+
+                if (choice < 0 || choice > injuredPlayers.Count)
+                {
+                    Console.WriteLine("Invalid choice. Try again.");
+                }
+                else if (choice == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    Person p = injuredPlayers[choice - 1];
+                    if (gameHandler.PlayerTeam.Budget < p.Cost / 2)
+                    {
+                        Console.WriteLine("Not enough budget to give medical attention.");
+                        return;
+                    }
+                    else
+                    {
+                        p.Status = "Active";
+                        p.Cost *= 2;
+                        if (p.Value < 0)
+                        {
+                            p.Value *= -2;
+                        }
+                        else
+                        {
+                            p.Value *= 2;
+                        }
+                        Console.WriteLine($"{p.Name} is now active.");
+                        return;
+                    }
+                }
+            }
+        }
+
+        public Stack<Person> ChooseLineup()
         {
 
             List<Person> lineupList = [.. gameHandler.PlayerTeam.Players];
